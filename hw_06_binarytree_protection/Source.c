42d91143d0e12f115844671a4f9b732a640ed4e7
Semchenock <64313471+Semchenock@users.noreply.github.com>
2020-10-10 12:58:57 +0300

    prot.

---
+++ hw_06_binarytree_protection/Source.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "binarytree.h"
+int main()
+{
+	struct node* root = NULL;
+	int menu = -1, value, i, cout=0;
+	while (menu != 0)
+	{
+		printf("1 add new \n");
+		printf("2 breadth first search \n");
+		printf("3 depth first search \n");
+		printf("4 search number \n");
+		printf("5 count equal \n");
+		printf("0 exit \n");
+		printf("Enter number: ");
+		scanf("%d", &menu);
+		if (menu == 1)
+		{
+			printf("Enter your number: ");
+			scanf("%d", &value);
+			add_new(&root, value);
+		}
+		else if (menu == 2)
+		{
+			bfs(root);
+		}
+		else if (menu == 3)
+		{
+			dfs(root);
+			printf("\n");
+		}
+		else if (menu == 4)
+		{
+			printf("Enter your number: ");
+			scanf("%d", &value);
+			search(root, value);
+		}
+		else if (menu == 5)
+		{
+			cout=0;
+			printf("Count of equal: %d\n", count_eq(root, cout));
+		}
+		else if (menu == 0)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("Wrong number\n");
+		}
+	}
+	return 0;
+}
